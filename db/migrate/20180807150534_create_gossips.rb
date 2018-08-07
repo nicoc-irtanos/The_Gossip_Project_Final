3bdfbbd34728217532ca60ce6ec5522419903d91
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-07 18:38:33 +0200

    Working on navbar

---
+++ db/migrate/20180807150534_create_gossips.rb
@@ -0,0 +1,8 @@
+class CreateGossips < ActiveRecord::Migration[5.2]
+  def change
+    create_table :gossips do |t|
+      t.text :content
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  Gossip.create!(content: Faker::ChuckNorris.fact, user_id: 1)
+end
